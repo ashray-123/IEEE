aee796beb190c80139e001768ce85433b677d7c8
Bharath <bharath1234gowda@gmail.com>
2021-12-11 12:51:29 +0530

    something

--- ieee/angular/src/app/dashboard/dashboard.component.ts
+++ ieee/angular/src/app/dashboard/dashboard.component.ts
@@ -9,18 +9,38 @@ import { GetDataService } from '../services/get-data.service';
 export class DashboardComponent implements OnInit {
 
   company_name:any="";
+  total_article:number=0;
+  positive_article=0;
+  negative_article=0;
+  neutral_article=0;
   constructor(private route:ActivatedRoute,private _service:GetDataService) { }
 
   async ngOnInit(): Promise<void> {
     this.company_name=this.route.snapshot.paramMap.get("key")
     console.log(this.company_name)
     var data=await this._service.searchCompanyAnalysis(encodeURI(this.company_name))
+    var analyzed_data=[]
+    var nonanalyzed_data=[]
     for(var art of data){
       if(art.subjectivity==0.0 && art.positive==0 && art.negative==0 && art.negative==0){
-        continue
+        nonanalyzed_data.push(art)
       }
-      console.log(art)
+      else{
+        analyzed_data.push(art)
+        if(art.positive>art.negative && art.positive>art.neutral){
+          this.positive_article++
+        }
+        else if (art.negative>art.positive && art.negative>art.neutral){
+          this.negative_article++
+        }
+        else if (art.neutral>art.negative && art.neutral>art.positive){
+          this.neutral_article++
+        }
+      }
+      
     }
+    this.total_article=analyzed_data.length;
   }
-
+    // console.log(nonanalyzed_data)  }
+  
 }
