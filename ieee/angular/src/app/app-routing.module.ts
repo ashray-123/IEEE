269d1f2c8c685cdd16bebe144b94cd9355ff18a1
Bharath <bharath1234gowda@gmail.com>
2021-12-11 10:37:19 +0530

    routing

--- ieee/angular/src/app/app-routing.module.ts
+++ ieee/angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"search",component:SearchPageComponent},
+  {path:"viewstats/:key",component:DashboardComponent},
+  {path:"**",component:SearchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
